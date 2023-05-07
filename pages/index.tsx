e16150bac6730cc0e9c298083a0a538ca7f47703
pangilinan-patrick <97028950+pangilinan-patrick@users.noreply.github.com>
2023-05-07 10:05:21 +0800

    center elements with flex

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,16 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className={`flex justify-center items-center h-screen`}>
-      <div>
+    <main>
+      <div
+        className={`flex justify-center items-center h-screen flex-col mx-20`}
+      >
         {/* input and button */}
         <div className={`mb-3`}>
           <input
             type="text"
             placeholder="Add Todo"
-            className={`p-1 rounded-md`}
+            className={`p-1 rounded-md mr-1 my-2`}
           />
           <button className={`bg-cyan-900 p-2 m-1 rounded-md`}>Add Todo</button>
         </div>
@@ -22,7 +24,9 @@ export default function Home() {
           <div
             className={`flex justify-center items-center bg-cyan-700 rounded-md`}
           >
-            Take care of cats.
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type.
           </div>
         </div>
       </div>
